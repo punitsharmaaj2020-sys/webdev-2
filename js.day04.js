4548ef27afcd43a58a92450d5c42f3d34b0ecc97
unknown <punitsharmaaj2020-sys>
2026-01-22 14:14:03 +0530

    js file

---
+++ js.day04.js
@@ -0,0 +1,7 @@
+let user={
+    fullName: "punit",
+    address: "Gurugram",
+    mobileNo: 1234567890,
+    favColor: ["black","red","green"]
+}
+console.log(user.fullName,user.address,user.mobileNo,user.favColor)
