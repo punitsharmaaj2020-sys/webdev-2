cf9d5378caa6f39c0e9b551d75f4a497612ab3c3
unknown <punitsharmaaj2020-sys>
2026-02-01 14:05:05 +0530

    js session

---
+++ day05/js.js
@@ -0,0 +1,7 @@
+ var x = 5;
+console.log(x);
+function test() {
+    let y = 10;
+    console.log(y);
+}
+test();
---
+++ task01/script.js
@@ -0,0 +1,5 @@
+const profileSection = document.getElementById("profile");
+  
+profileSection.style.backgroundColor = "blue";
+profileSection.style.padding = "10px";
+ profileSection.style.textAlign = "center";
